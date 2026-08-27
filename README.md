f9a12c997769d931ab35c332c4da92d117769ae1
Marcus Behel <marcus.behel@mindspring.com>
2026-08-26 21:01:27 -0400

    Create linuxdeps script

--- README.md
+++ README.md
@@ -33,25 +33,10 @@ Experiments with using QT QML and rust
 
 2. Install required C++ libraries using `vcpkg`
     ```sh
-    # On Linux, first, install required system packages. These lists may be incomplete
-    # Ubuntu:
-    sudo apt install build-essential git curl zip unzip tar pkg-config \
-            libgl1-mesa-dev libglu1-mesa-dev \
-            '^libxcb.*-dev' libx11-xcb-dev libxrender-dev libxi-dev \
-            libxkbcommon-dev libxkbcommon-x11-dev
+    # On Linux, install build deps using system package manager
+    ./linuxdeps.sh
 
-
-    # Fedora:
-    sudo dnf install @development-tools
-    sudo dnf install git curl zip unzip tar pkgconfig \
-            mesa-libGL-devel mesa-libGLU-devel \
-            libxcb-devel xcb-util-devel xcb-util-image-devel xcb-util-keysyms-devel \
-            xcb-util-renderutil-devel xcb-util-wm-devel libX11-xcb \
-            libXrender-devel libXi-devel libxkbcommon-devel libxkbcommon-x11-devel
-
-
-
-    # All OSes (including Windows and macOS)
+    # All OSes
     ./cargow install cargo-vcpkg
     ./cargow vcpkg -v build
     ```
---
+++ linuxdeps.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+
+case "$(uname -o)" in
+    GNU/Linux)
+        ;;
+    *)
+        echo "$(basename "$0") is only needed on Linux distros"
+        exit 1
+        ;;
+esac
+
+
+DISTRO="$(cat /etc/os-release | grep ^ID= | sed 's/^ID=//g')"
+case "$DISTRO" in
+    ubuntu|debian)
+        (
+            set -x
+            sudo apt install build-essential git curl zip unzip tar \
+                pkg-config libgl1-mesa-dev libglu1-mesa-dev '^libxcb.*-dev' \
+                libx11-xcb-dev libxrender-dev libxi-dev libxkbcommon-dev \
+                libxkbcommon-x11-dev
+        )
+        ;;
+    fedora|rhel|almalinux|rocky)
+        (
+            set -x
+            sudo dnf install @development-tools git curl zip unzip \
+                tar pkgconfig mesa-libGL-devel mesa-libGLU-devel \
+                libxcb-devel xcb-util-devel xcb-util-image-devel \
+                xcb-util-keysyms-devel xcb-util-renderutil-devel \
+                xcb-util-wm-devel libX11-xcb libXrender-devel libXi-devel \
+                libxkbcommon-devel libxkbcommon-x11-devel gcc-c++ \
+                autoconf autoconf-archive automake libtoolize cmake \
+                ninja-build perl-open libXi-devel libXtst-devel
+        )
+        ;;
+esac
+
